28683f84d6e55883767996890f73fe79cfebc8aa
Stephanie Ferreira <120394202+stephaniefv@users.noreply.github.com>
2023-02-08 09:17:51 -0300

    Update match.py

--- match.py
+++ match.py
@@ -1,9 +1,29 @@
-#Proposta do projeto individual 1
-#Stephanie Ferreira Vale
-#################################
-#Deu Macht !!!!
-
-#Explicando o Código
+##################################################################
+#  SENAC/RESILIA - Formação em Análise de Dados (FAD)            #
+#  Projeto Individual 1 - Módulo 2 - Deu Match!                  #
+#  !/usr/bin/env python3                                         #
+#  -*- coding: utf-8 -*-                                         #
+#  Criado por: Stephanie Ferreira Vale                  #
+#  Data de criação: 07/02/2023                                   #
+#  versão = '3.11(64-bit)'                                       #
+##################################################################
+#
+# Atividade:
+#
+# Uma startup está desenvolvendo um aplicativo que verifica a compatibilidade
+# de um candidato com uma vaga de acordo com seu resultado nas etapas do
+# processo seletivo.
+# Para isso foi criado um teste que devolve uma string no seguinte formato:
+# eX_tX_pX_sX (sendo que cada X é substituído pela avaliação da pessoa em
+# uma das etapas do processo - entrevista, teste teórico, teste prático e
+# avaliação de soft skills).
+#
+# Criar com Python uma lista para armazenar esses resultados
+# (e outros resultados que quiser no mesmo formato; o código
+# precisa funcionar para qualquer lista que seja inserida nesse
+# formato) e depois uma função que busca o candidato de
+# acordo com os critérios digitados pelo usuário.
+#
 
 def add_cand(): 
 
